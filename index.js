291153417b9a39a43ee509f66e4f3cb7424ab6ce
thomassupervil <thomas.supervil@gmail.com>
2021-06-02 10:56:49 +0200

    add route to get and update data of annonce table

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 const express = require('express');
+const connection = require('./db-config');
 // const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected as id ${connection.threadId}`);
+  }
+});
+
+app.get('/annonce/:id', (req, res) => {
+  const annId = req.params.id;
+  connection.query(
+    'SELECT * FROM annonce WHERE id = ?',
+    [annId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send('Error retrieving annonce from database');
+      } else if (results.length) res.json(results[0]);
+      else res.status(404).send('Annonce not found');
+    }
+  );
+});
+
+app.put('/annonce/:id', (req, res) => {
+  const annId = req.params.id;
+
+  connection.query(
+    'SELECT * FROM annonce WHERE id =?',
+    [annId],
+    (err, selectResults) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error updating annonce');
+      } else {
+        const annFromDb = selectResults[0];
+        if (annFromDb) {
+          const annPropToUpdate = req.body;
+          connection.query(
+            'UPDATE annonce SET ? WHERE id=?',
+            [annPropToUpdate, annId],
+            (error) => {
+              if (error) {
+                console.log(error);
+                res.status(500).send('error updating the annonce');
+              } else {
+                const updated = { ...annFromDb, ...annPropToUpdate };
+                res.status(200).json(updated);
+              }
+            }
+          );
+        } else {
+          res.status(404).send(`Movie with id ${annId} not found`);
+        }
+      }
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
