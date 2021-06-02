46fcf8248bd3c7f9075bc528fb1131def7361bd0
frvanco <antoinevancompernolle@gmail.com>
2021-06-02 16:36:31 +0200

    merge

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connection = require('./db-config');
 // const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 connection.connect((err) => {
   if (err) {
