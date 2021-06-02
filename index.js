60ab1d08d2586c758e570b7ae63a26d9f69bcbb9
thomassupervil <thomas.supervil@gmail.com>
2021-06-02 14:49:15 +0200

    add delete route

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const connection = require('./db-config');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.post('/annonce', (req, res) => {
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected as id ${connection.threadId}`);
+  }
+});
+
+app.post('/annonce', async (req, res) => {
   const {
     id,
     district,
@@ -22,7 +30,7 @@ app.post('/annonce', (req, res) => {
     describe,
     idUser,
   } = req.body;
-  connection.query(
+  await connection.query(
     'INSERT INTO users (id, district, adress, city, furnished, rent, surface, animals, title, category, type, energyClass,rooms, describe,id_user) VALUES (?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?,?)',
     [
       id,
@@ -50,18 +58,6 @@ app.post('/annonce', (req, res) => {
       }
     }
   );
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-  } else {
-    console.log(`connected as id ${connection.threadId}`);
-  }
 });
 
 app.get('/annonce/:id', async (req, res) => {
