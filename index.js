58b83108cb16f42e4a4fd2ebe75ee21564b6f442
frvanco <antoinevancompernolle@gmail.com>
2021-06-02 14:50:30 +0200

    add way

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const connection = require('./db-config');
 const app = express();
 const port = process.env.PORT || 3000;
 
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected as id ${connection.threadId}`);
+  }
+});
+
 app.post('/annonce', (req, res) => {
   const {
     id,
@@ -50,18 +58,6 @@ app.post('/annonce', (req, res) => {
       }
     }
   );
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-  } else {
-    console.log(`connected as id ${connection.threadId}`);
-  }
 });
 
 app.get('/annonce/:id', (req, res) => {
