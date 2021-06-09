3072246b75c9013e65e7281b6ee2049548d223e2
Kévin <bdtkevin@gmail.com>
2021-06-09 11:55:14 +0200

    add post route users

--- models/users.js
+++ models/users.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const db = require('../conf');
 
 const getAll = () => {
@@ -12,7 +13,62 @@ const getOne = (id) => {
   return db.query(sql, [id]);
 };
 
+const create = ({
+  name,
+  firstname,
+  email,
+  password,
+  credits,
+  city,
+  gender,
+  budget,
+  age,
+  animals,
+  aboutme,
+  hobbies,
+  telephone,
+}) => {
+  const sql =
+    'INSERT INTO users (name, firstname, email, password, credits, city, gender, budget, age, animals, aboutme, hobbies, telephone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  return db.query(sql, [
+    name,
+    firstname,
+    email,
+    password,
+    credits,
+    city,
+    gender,
+    budget,
+    age,
+    animals,
+    aboutme,
+    hobbies,
+    telephone,
+  ]);
+};
+
+const validate = (data, forCreation = true) => {
+  const presence = forCreation ? 'required' : 'optional';
+  return Joi.object({
+    name: Joi.string().max(64).presence(presence),
+    firstname: Joi.string().max(32).presence(presence),
+    email: Joi.string().max(64).email().presence(presence),
+    password: Joi.string().max(64).presence(presence),
+    credits: Joi.number().integer().min(0).presence(presence),
+    city: Joi.string().max(32).presence(presence),
+    gender: Joi.boolean().presence(presence),
+    budget: Joi.number().integer().min(0).presence(presence),
+    age: Joi.number().integer().min(0).presence(presence),
+    animals: Joi.boolean().presence(presence),
+    aboutme: Joi.string().max(256).presence(presence),
+    hobbies: Joi.string().max(256).presence(presence),
+    telephone: Joi.string().max(20).presence(presence),
+  }).validate(data, { abortEarly: false }).error;
+};
+
 module.exports = {
   getAll,
   getOne,
+  create,
+  validate,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const User = require('../models/users');
 
 usersRouter.get('/', async (req, res) => {
   try {
-    const [results] = await User.getAll();
-    res.status(200).json(results);
+    const [user] = await User.getAll();
+    res.status(200).json(user);
   } catch {
-    res.status(400).send('error');
+    res.status(500).send('Error retrieving users from database');
   }
 });
 
@@ -15,12 +15,29 @@ usersRouter.get('/:id', async (req, res) => {
     const [user] = await User.getOne(req.params.id);
 
     if (user.length > 0) {
-      res.status(200).send(user);
+      res.status(200).json(user);
     } else {
-      res.status(404).send('User not found');
+      res.status(404).send(`User ${req.params.id} not found`);
     }
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send('Error retrieving users from database');
+  }
+});
+
+usersRouter.post('/', async (req, res) => {
+  const error = User.validate(req.body, true);
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    try {
+      const [result] = await User.create(req.body);
+      res.status(201).json({
+        id: result.insertId,
+        ...req.body,
+      });
+    } catch (err) {
+      res.status(500).send('Error saving the user');
+    }
   }
 });
 
