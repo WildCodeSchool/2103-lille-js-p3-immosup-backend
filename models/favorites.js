e561dd73d9a228743b5142840e35ea4b810aa5be
Zamomako <robin-delannoy_student2021@wilder.school>
2021-06-16 16:49:19 +0200

    create the route for favorites

---
+++ models/favorites.js
@@ -0,0 +1,23 @@
+const db = require('../conf');
+
+const getFav = () => {
+  const sql = `
+  select accomodations.*, (
+		select url from photos where idAccomodation=accomodations.id limit 1
+	) as url
+from 
+	favorites
+    join accomodations on favorites.idAccomodation=accomodations.id
+    join photos on photos.idAccomodation=accomodations.id
+where
+	favorites.idUser=1
+group by id
+
+  `;
+
+  return db.query(sql);
+};
+
+module.exports = {
+  getFav,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const usersRouter = require('./users');
+const favoritesRouter = require('./favorites');
 
 const setupRoutes = (app) => {
   app.use('/api/users', usersRouter);
+  app.use('/api/favorites', favoritesRouter);
 };
 
 module.exports = {
