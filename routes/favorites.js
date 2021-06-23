9f30ee602e361a83d1c548be26f5f4039d5cda29
Zamomako <robin-delannoy_student2021@wilder.school>
2021-06-23 10:30:49 +0200

    Favorites routes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,21 +1,6 @@
 const favoritesRouter = require('express').Router();
 const favorite = require('../models/favorites');
 
-favoritesRouter.get('/', async (req, res) => {
-  try {
-    const [fav] = await favorite.getFav(req.query);
-
-    if (!fav) {
-      res.status(404).send(`No ${req.query} not found`);
-    } else {
-      res.status(200).json(fav);
-    }
-  } catch (err) {
-    res.status(500).send('Error retrieving users from database');
-  }
-});
-
-/*
 favoritesRouter.get('/:id', async (req, res) => {
   try {
     const [fav] = await favorite.getFav(req.params.id);
@@ -27,9 +12,7 @@ favoritesRouter.get('/:id', async (req, res) => {
     }
   } catch (err) {
     res.status(500).send('Error retrieving users from database');
-    console.log(err);
   }
 });
-*/
 
 module.exports = favoritesRouter;
