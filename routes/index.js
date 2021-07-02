52bcb13320172647f031c42b9118000ae873f472
Sofia boud <sofiasoul@hotmail.fr>
2021-07-02 17:46:02 +0200

    delete annonce

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const mailsRouters = require('./mails');
 const usersRouter = require('./users');
-const annonceRouter = require('./annonce');
 
 const setupRoutes = (app) => {
   app.use('/access', mailsRouters);
   app.use('/users', usersRouter);
-  app.use('/api/annonce', annonceRouter);
 };
 
 module.exports = {
