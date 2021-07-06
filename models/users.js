544621448e9e5e5da16d39745d30319e50584972
Kévin <bdtkevin@gmail.com>
2021-07-06 10:27:57 +0200

    update models users

--- models/users.js
+++ models/users.js
@@ -1,61 +1,40 @@
 const { db } = require('../conf');
 
 const getOneId = (id) => {
-  return db.query(
-    'SELECT users.id, name, firstname, email, credits, city, gender, budget, age, animals, aboutme, hobbies, telephone, photos.url FROM users JOIN photos ON photos.idUser = users.id WHERE users.id = ?',
-    [id]
-  );
+  const sql = `SELECT id, lastname, firstname, email, credits, city, gender, budget, birthday, animals, aboutme, hobbies, phone, avatar_url
+              FROM users
+              WHERE id = ?`;
+
+  return db.query(sql, [id]);
 };
 
 const getOneEmail = (email) => {
-  return db.query(
-    'SELECT users.id, name, firstname, email, credits, city, gender, budget, age, animals, aboutme, hobbies, telephone, photos.url FROM users JOIN photos ON photos.idUser = users.id WHERE users.email = ?',
-    [email]
-  );
+  const sql = `SELECT id, lastname, firstname, email, credits, city, gender, budget, birthday, animals, aboutme, hobbies, phone, avatar_url
+              FROM users
+              WHERE email = ?`;
+
+  return db.query(sql, [email]);
 };
 
 const getAll = () => {
-  return db.query(
-    'SELECT users.id, name, firstname, email, credits, city, gender, budget, age, animals, aboutme, hobbies, telephone, photos.url FROM users JOIN photos ON photos.idUser = users.id'
-  );
+  const sql = `SELECT id, lastname, firstname, email, credits, city, gender, budget, birthday, animals, aboutme, hobbies, phone, avatar_url
+              FROM users`;
+
+  return db.query(sql);
 };
 
-const create = ({
-  name,
-  firstname,
-  email,
-  password,
-  credits,
-  city,
-  gender,
-  budget,
-  age,
-  animals,
-  aboutme,
-  hobbies,
-  telephone,
-}) => {
-  const sql =
-    'INSERT INTO users (name, firstname, email, password, credits, city, gender, budget, age, animals, aboutme, hobbies, telephone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  return db.query(sql, [
-    name,
-    firstname,
-    email,
-    password,
-    credits,
-    city,
-    gender,
-    budget,
-    age,
-    animals,
-    aboutme,
-    hobbies,
-    telephone,
-  ]);
+const create = (user) => {
+  const sql = `INSERT INTO users
+              SET ?`;
+
+  return db.query(sql, user);
 };
 
 const update = (id, newAttributes) => {
-  const sql = 'Update users SET ? WHERE id = ?';
+  const sql = `UPDATE users
+              SET ?
+              WHERE id = ?`;
+
   return db.query(sql, [newAttributes, id]);
 };
 
