a9e814b0dc582db380f1b247ef8b58d35a6c5f1a
hochart.dev <hochart.dev@gmail.com>
2021-07-15 16:31:10 +0200

    add pictures in gitignore , add route in upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -2,13 +2,20 @@ const upload = require('express-fileupload');
 const fs = require('fs');
 const uploadRouter = require('express').Router();
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { db } = require('../conf');
 
 uploadRouter.use(express.static('./pictures'));
 uploadRouter.use(express.static('pictures'));
 
 uploadRouter.use(upload());
 uploadRouter.post('/', (req, res) => {
-  fs.writeFile('pictures/toto2.jpg', req.files.image.data, () => {
+  const fileName = `pictures/${uuidv4()}.jpeg`;
+  fs.writeFile(fileName, req.files.image.data, () => {
+    db.query(
+      'INSERT INTO photos (url, idUser , idAccomodation) VALUES (?, ?, ?)',
+      [fileName, 8, null]
+    );
     res.send('yay');
   });
 });
